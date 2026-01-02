bee57e1570480e8904edf877e94677813a75c0d9
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2026-01-02 22:30:02 +0700

    feat: Create basic layout with Header, Sidebar, Main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <header className="header">
+        <h1>My Product Management App</h1>
+      </header>
+      
+      <div className="main-layout">
+        <aside className="sidebar">
+          <h2>Sidebar</h2>
+          <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Profile</li>
+            <li>Login</li>
+          </ul>
+        </aside>
+        
+        <main className="content">
+          <h2>Welcome to Main Content</h2>
+          <p>Đây là nơi hiển thị nội dung chính.</p>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
