6f9f0916680f125f3ab86682eafe07e3c2af1a3e
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2026-01-02 23:40:32 +0700

    feat: Add useEffect for document title and mock product data

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
-  const menuItems = ['Home', 'Products', 'Profile', 'Login', 'Settings'];
+  const menuItems = ['Home', 'Products', 'Profile', 'Login', 'Settings', 'Logout'];
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  useEffect(() => {
+    document.title = isSidebarOpen ? "Sidebar is Open" : "Sidebar is Closed";
+  }, [isSidebarOpen]);
+
 
   return (
     <div className="app">
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function MainContent() {
     const [count, setCount] = useState(0);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setProducts([
+                { id: 1, name: 'Product A', price: 100 },
+                { id: 2, name: 'Product B', price: 150 },
+                { id: 3, name: 'Product C', price: 200 },
+            ]);
+        }, 1000);
+    }, []);
+
   return (
     <main className="content">
-      <h2>Welcome to Main Content</h2>
-      <p>Đây là nơi hiển thị nội dung chính.</p>
+      <h2>Danh sách sản phẩm</h2>
+        {products.length === 0 ? (
+            <p>Đang tải sản phẩm...</p>
+        ) : (
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                        {product.name} - {product.price.toLocaleString('vi-VN')} VND
+                    </li>
+                ))}
+            </ul>
+        )}
+
+
       <p>Bạn đã click nút tăng: <strong>{count}</strong> lần </p>
       <button onClick={() => setCount(count + 1)}> Tăng count </button>
     </main>
